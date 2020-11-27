5eb59d9406b846e87677430c0d66b9468350696d
wxm <wxm@guigu.com>
2020-11-27 16:00:51 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @ProjectName git0720
+ * @Description: TODO
+ * @Author xmWang
+ * @Date 2020-11-27-15:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("This is V1");
+    }
+}
